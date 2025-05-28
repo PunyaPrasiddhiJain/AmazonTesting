df64c9f6a81d90543c57140c29dc36cdcf9c58ab
punya.jain <punya.jain@192.168.3.1>
2025-05-28 11:52:51 +0530

    Next Commit

--- src/test/java/TestAmazon_/AmazonSignin.java
+++ src/test/java/TestAmazon_/AmazonSignin.java
@@ -1,65 +1,60 @@
 package TestAmazon_;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-//import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.Actions;
+
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.*;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
-import java.util.Scanner;
+import java.util.List;
 
-public class AmazonSignin extends BaseTest{
+public class AmazonSignin extends BaseTest {
 
-  //  WebDriver driver;
-    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-
-//    @BeforeClass
-//    public void setUp() {
-//        driver = new FirefoxDriver();
-//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-//        driver.manage().window().maximize();
-//        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-//    }
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+    JavascriptExecutor js = (JavascriptExecutor) driver;
 
     @Test
-    public void signInToAmazon() throws InterruptedException {
-        // Navigate to the provided Amazon sign-in URL
-        driver.get("https://www.amazon.in/");
-        WebElement accountLists = driver.findElement(By.id("nav-link-accountList"));
-        Actions actions = new Actions(driver);
-        actions.moveToElement(accountLists).perform();
-        
-        
-        
-        driver.findElement(By.id("nav-link-accountList-nav-line-1")).click();
+    public void signInAndAddToCart() throws InterruptedException {
+    	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
 
-//        WebElement signInLink = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#nav-flyout-ya-signin a")));
-//        signInLink.click();
-        
-        //Thread.sleep(5000);
-        
-        // Step 1: Enter email or phone number
-        driver.findElement(By.id("ap_email_login")).sendKeys("punyajain202@gmail.com");
-        driver.findElement(By.className("a-button-input")).click();
+        driver.get("https://www.amazon.in/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.in%2F%3Fref_%3Dnav_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=inflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0");
 
-        // Step 2: Enter password
+        // Sign in
+        driver.findElement(By.id("ap_email")).sendKeys("punyajain202@gmail.com");
+        driver.findElement(By.id("continue")).click();
         driver.findElement(By.id("ap_password")).sendKeys("MYparant@2002");
         driver.findElement(By.id("signInSubmit")).click();
 
-//        Scanner scn =new Scanner(System.in);
-//        int code = scn.nextInt();
-//        
-//        driver.findElement(By.id("input-box-otp")).sendKeys(""+code);
-//        driver.findElement(By.id("cvf-submit-otp-button-announce")).click();
-    }
+        Thread.sleep(20000); // If OTP required manually
 
-//    @AfterClass
-//    public void tearDown() {
-//       driver.quit();
-//    }
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Laptop");
+        driver.findElement(By.id("nav-search-submit-button")).click();
+
+        // Wait and click the first product (open in same tab)
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".widgetId\\=search-results_1 > div:nth-child(1) > div:nth-child(1) > span:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > a:nth-child(2)")));
+
+        List<WebElement> products = driver.findElements(By.cssSelector(".widgetId\\=search-results_1 > div:nth-child(1) > div:nth-child(1) > span:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > a:nth-child(2)"));
+        if (!products.isEmpty()) {
+            WebElement firstProduct = products.get(0);
+
+            // Remove target="_blank" to avoid new tab
+            js.executeScript("arguments[0].removeAttribute('target')", firstProduct);
+            firstProduct.click();
+        } else {
+            System.out.println("No products found");
+            return;
+        }
+
+        // Add to cart
+        driver.findElement(By.xpath("/html/body/div[2]/div/div/div[15]/div/div/div/div[1]/div[2]/div[1]/div[2]/div/form/div[6]/span/span/input")).click();
+
+        // Optional: navigate to cart
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("nav-cart"))).click();
+
+        System.out.println("Product added to cart successfully.");
+    }
 }
+
+//driver.get("https://www.amazon.in/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.in%2F%3Fref_%3Dnav_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=inflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0");
--- src/test/java/com/TestAmazonCucumber/AmazonSteps.java
+++ src/test/java/com/TestAmazonCucumber/AmazonSteps.java
@@ -20,16 +20,17 @@ public class AmazonSteps {
         driver = new FirefoxDriver();
         wait = new WebDriverWait(driver, Duration.ofSeconds(15));
         driver.manage().window().maximize();
-        driver.get("https://www.amazon.in/ap/register");
+        driver.get("https://www.amazon.in/ap/register?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.in%2Fgp%2Fcss%2Fhomepage.html%2F%3Fie%3DUTF8%26ref_%3Dnav_newcust&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=inflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0");
     }
 
     @When("I fill out the registration form")
-    public void i_fill_out_the_registration_form() {
+    public void i_fill_out_the_registration_form() throws InterruptedException {
         driver.findElement(By.id("ap_customer_name")).sendKeys("TestUser");
-        driver.findElement(By.id("ap_email")).sendKeys("testuser123@example.com");
+        driver.findElement(By.id("ap_phone_number")).sendKeys("8770533132");
         driver.findElement(By.id("ap_password")).sendKeys("Test@123");
-        driver.findElement(By.id("ap_password_check")).sendKeys("Test@123");
+        //driver.findElement(By.id("ap_password_check")).sendKeys("Test@123");
         driver.findElement(By.id("continue")).click();
+        Thread.sleep(30000);
     }
 
     @Then("I should be redirected to the OTP verification page")
@@ -49,43 +50,50 @@ public class AmazonSteps {
     }
 
     @When("I sign in with valid credentials")
-    public void i_sign_in_with_valid_credentials() {
+    public void i_sign_in_with_valid_credentials() throws InterruptedException {
         Actions actions = new Actions(driver);
         WebElement accountList = driver.findElement(By.id("nav-link-accountList"));
         actions.moveToElement(accountList).perform();
         WebElement signInLink = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#nav-flyout-ya-signin a")));
         signInLink.click();
 
-        driver.findElement(By.id("ap_email")).sendKeys("youremail@example.com");
-        driver.findElement(By.id("continue")).click();
-        driver.findElement(By.id("ap_password")).sendKeys("YourPassword123");
+        driver.findElement(By.xpath("//*[@id=\"ap_email_login\"]")).sendKeys("punyajain202@gmail.com");
+        driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div/div/div/div/span/form/span/span/input")).click();
+        driver.findElement(By.id("ap_password")).sendKeys("MYparant@2002");
         driver.findElement(By.id("signInSubmit")).click();
+        Thread.sleep(10000);
     }
 
     @Then("I should be logged in")
     public void i_should_be_logged_in() {
         // Check some post-login element
         System.out.println("Logged in successfully.");
+        driver = null;
     }
 
     @When("I search for {string}")
     public void i_search_for_product(String product) {
-        WebElement searchBox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("twotabsearchtextbox")));
-        searchBox.sendKeys(product);
+    	driver.findElement(By.id("twotabsearchtextbox")).sendKeys(product);
         driver.findElement(By.id("nav-search-submit-button")).click();
     }
 
     @When("I add the first product to the cart")
     public void i_add_first_product_to_cart() {
-        WebElement firstProduct = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div.s-main-slot div[data-component-type='s-search-result'] h2 a")));
-        firstProduct.click();
+        // Wait for the first product link
+        WebElement firstProductLink = wait.until(ExpectedConditions.elementToBeClickable(
+            By.cssSelector("div.s-main-slot div[data-component-type='s-search-result'] h2 a")));
 
-        ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
-        driver.switchTo().window(tabs.get(1));
+        // Remove target="_blank" using JavaScript so it opens in the same tab
+        ((JavascriptExecutor) driver).executeScript("arguments[0].removeAttribute('target');", firstProductLink);
 
+        // Click the link
+        firstProductLink.click();
+
+        // Now add to cart (in same tab)
         WebElement addToCart = wait.until(ExpectedConditions.elementToBeClickable(By.id("add-to-cart-button")));
         addToCart.click();
     }
+
 
     @When("I click Buy Now")
     public void i_click_buy_now() {
